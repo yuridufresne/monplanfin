b18372b125b7fb3abeae8a1bdae02824b270db6b
yuridufresne <yuridufresne@gmail.com>
2026-06-10 16:18:31 -0400

    Add InsightsABF.jsx component

---
+++ src/components/abf/InsightsABF.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function InsightsABF() {
+  return (
+    <div>
+      <h1>InsightsABF Component</h1>
+      <p>Component content goes here</p>
+    </div>
+  );
+}
