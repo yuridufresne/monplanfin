3d7c32d5c9f2a2b4fad7554f8ecdadb31fab6c0a
yuridufresne <yuridufresne@gmail.com>
2026-06-14 20:52:27 -0400

    Create EducationFinanciere page component

---
+++ src/pages/EducationFinanciere.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/EducationFinanciere.css';
+
+const EducationFinanciere = () => {
+  return (
+    <div className="education-financiere">
+      <h1>Éducation Financière</h1>
+      <p>Contenu d'éducation financière à venir</p>
+    </div>
+  );
+};
+
+export default EducationFinanciere;
