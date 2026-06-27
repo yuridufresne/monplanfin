40f28d91bd6c918e6329d20d888fec6bfea5866d
Yuri Dufresne <coachyu@MacBook-Air-de-Yuri.local>
2026-06-27 19:06:34 -0400

    fix: Supabase credentials en dur

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ''
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
+const supabaseUrl = 'https://ljezchpqqyxgehdwvnot.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqZXpjaHBxcXl4Z2VoZHd2bm90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1ODMxMjYsImV4cCI6MjA3ODE1OTEyNn0.nkPqjw02mTAJxuh7qsfU2ubk0aROsavC3Jo5Al0ZMWs'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
