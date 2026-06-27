9279d517778d43b954c1ab46a2242d0adc51b505
Yuri Dufresne <coachyu@air-de-yuri.home>
2026-06-27 03:03:56 -0400

    feat: ErrorBoundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ErrorBoundary from "@/components/ErrorBoundary";
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
