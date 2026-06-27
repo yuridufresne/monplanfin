7b5881de556543ae3cc5b537371c852025810c6b
Yuri Dufresne <coachyu@MacBook-Air-de-Yuri.local>
2026-06-27 19:13:20 -0400

    fix: update Supabase anon key

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ljezchpqqyxgehdwvnot.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqZXpjaHBxcXl4Z2VoZHd2bm90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1ODMxMjYsImV4cCI6MjA3ODE1OTEyNn0.nkPqjw02mTAJxuh7qsfU2ubk0aROsavC3Jo5Al0ZMWs'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqZXpjaHBxcXl4Z2VoZHd2bm90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NzEwNjMsImV4cCI6MjA5ODE0NzA2M30.1-5F2Sndy9iUEQQadEgfjJmnaubkV27xrZHjV-AKwmg'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
