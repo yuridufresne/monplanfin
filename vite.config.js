f9730808984e62e61100c74b121c4a6f31aa117c
Yuri Dufresne <coachyu@mac.home>
2026-06-27 17:56:08 -0400

    fix: remove base44 vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
-import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   logLevel: 'error',
-  plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
+  plugins: [react(),
     react(),
   ],
   build: {
